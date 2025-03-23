43b913c09834ae49d44f1307775f4011373df484
OmAmar <bt23cse106@iiitn.ac.in>
2025-03-23 15:04:29 +0530

    stress_testing improved

--- Stress_Testing/stress_checker.py
+++ Stress_Testing/stress_checker.py
@@ -5,54 +5,48 @@ import msvcrt
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 AC = os.path.join(current_dir, "wa.py")
-Brute = os.path.join(current_dir, "brute.py")
-
-def generate_random_input_temp():
-    n = random.randint(1, 10)
-    arr = [random.randint(1, n) for _ in range(n)]
-    input_data = "1\n" + f"{n}\n" + " ".join(map(str, arr))
-    return input_data
-def generate_random_input():
-    n = random.randint(1, 10)
-    arr = [random.randint(1, n) for _ in range(n)]
-    input_data = "1\n" + f"{n}\n" + " ".join(map(str, arr))
-    return input_data
 
 local = True
 if local:
     from colorama import Fore, Back, Style, init
     init(autoreset=True)
 
+def check(input, output):
+    L = list(map(int, output.split(' ')))
+    input = int(''.join(input[2:]))
+    return True
+
+def generate_random_input_temp():
+    n = random.randint(1, 10)
+    arr = [random.randint(1, n) for _ in range(n)]
+    input_data = "1\n" + f"{n}\n" + " ".join(map(str, arr))
+    return input_data
+
+def generate_random_input():
+    n = random.randint(1, 10)
+    input_data = "1\n" + f"{n}\n"
+    return input_data
+
+random_input = True
+val = 1
 count = 0
 while True:
     if msvcrt.kbhit() and msvcrt.getch() == b'\r':
         break
-    input_data = generate_random_input()
+    if random_input:
+        input_data = generate_random_input()
+    else:
+        input_data = str(val)
+        val += 1
+
     input_file = "input.txt"
-    result_brute = subprocess.run(
-        ["python", Brute],
-        input=input_data,
-        text=True,
-        capture_output=True
-    )
+
     result_ac = subprocess.run(
         ["python", AC],
         input=input_data,
         text=True,
         capture_output=True
     )
-
-    if result_brute.returncode != 0:
-        if count:
-            print()
-        if local:
-            print(Back.YELLOW + " Brute force code encountered an error! ")
-        else:
-            print("Brute force code encountered an error!")
-        print("Input Data:")
-        print(input_data.strip())
-        print("Error Message:\n", result_brute.stderr)
-        exit()
 
     if result_ac.returncode != 0:
         if count:
@@ -62,14 +56,12 @@ while True:
         else:
             print("Your code encountered an error!")
         print("Input Data:")
-        print(input_data)
+        print(input_data.strip())
         print("Error Message:\n", result_ac.stderr)
         exit()
 
-    output_brute = result_brute.stdout.strip()
     output_ac = result_ac.stdout.strip()
-    
-    if output_brute != output_ac:
+    if not check(input_data, output_ac):
         if count:
             print()
         if local:
@@ -77,9 +69,8 @@ while True:
         else:
             print("Test Case Failed!")
         print("Input Data:")
-        print(input_data)
-        print("Expected Output:", output_brute)
-        print("Your Output:    ", output_ac)
+        print(input_data.strip())
+        print("Received Output: ", output_ac)
         with open(input_file, 'w') as file:
             file.write(input_data)
         exit()
